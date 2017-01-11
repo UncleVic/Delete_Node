d2f6bf7c18bfe135b4c91024d54d0b489373be3a
Viktor Polyakov <vpolyakov@synergy.ru>
2017-01-11 08:57:50 +0300

    Add script

---
+++ delete_node.sh
@@ -0,0 +1,14 @@
+rm -rf /usr/local/lib/node*
+rm -rf /usr/local/include/node*
+rm -rf /usr/local/bin/node*
+rm -rf ~/.npm*
+rm -rf ~/.node-gyp
+rm -rf /usr/local/share/man/man1/node*
+rm -rf /usr/local/lib/dtrace/node.d
+rm -rf /usr/local/bin/node*
+rm -rf /opt/local/bin/node
+rm -rf /opt/local/include/node
+rm -rf /opt/local/lib/node_modules
+rm -rf /usr/local/share/doc/node
+rm -rf /usr/local/share/systemtap/tapset/node.stp
+
